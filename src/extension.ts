62db70bf34ce8768c30efbdff1f1358d70757cc9
clice lee <cliceleee@gmail.com>
2024-12-07 22:21:49 +0900

    Feat: edit feature - Convert class=... to a blank space

--- src/extension.ts
+++ src/extension.ts
@@ -4,8 +4,6 @@ export function activate(context: vscode.ExtensionContext) {
   console.log("Tailwind ClassName Hider is now activated!");
 
   let disposable = vscode.commands.registerCommand("extension.toggleClassNames", () => {
-    vscode.window.showInformationMessage("Toggle Tailwind Class Names executed!");
-
     const editor = vscode.window.activeTextEditor;
 
     if (!editor) {
@@ -24,18 +22,10 @@ export function activate(context: vscode.ExtensionContext) {
         const endPos = document.positionAt(match.index + match[0].length);
         const range = new vscode.Range(startPos, endPos);
 
-        // Toggle className visibility
-        if (match[0].includes("/*hidden*/")) {
-          const newText = match[0].replace("/*hidden*/", "");
-          editBuilder.replace(range, newText);
-        } else {
-          const newText = match[0].replace('="', '="/*hidden*/');
-          editBuilder.replace(range, newText);
-        }
+        // 해당 부분을 공백으로 대체
+        editBuilder.replace(range, "");
       }
     });
-
-    vscode.window.showInformationMessage("Tailwind className visibility toggled!");
   });
 
   context.subscriptions.push(disposable);
